afcd3f99899192e4439e62ed50067b10f5b2e1cb
revoly afanasev <rewoly@mail.ru>
2024-10-15 22:12:51 +0900

    Построение сущностей

---
+++ Monitoring/src/main/java/com/example/Monitoring/EventsRepository.java
@@ -0,0 +1,6 @@
+package com.example.Monitoring;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface EventsRepository extends JpaRepository<EventsEntity, Long> {}
