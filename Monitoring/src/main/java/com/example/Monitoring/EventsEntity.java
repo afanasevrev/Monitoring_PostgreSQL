32d58fcfa4f0e7faa31f3edb444cd0f019217bfa
revoly afanasev <rewoly@mail.ru>
2024-10-15 21:33:47 +0900

    Построение сущностей

--- Monitoring/src/main/java/com/example/Monitoring/EventsEntity.java
+++ Monitoring/src/main/java/com/example/Monitoring/EventsEntity.java
@@ -1,7 +1,6 @@
 package com.example.Monitoring;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 @Entity
@@ -9,5 +8,20 @@ import lombok.Setter;
 @Setter
 @Table(name = "events_entity")
 public class EventsEntity {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "current_date")
+    private String current_date;
+    @Column(name = "user")
+    private String user;
+    @Column(name = "action")
+    private String action;
+    public EventsEntity() {}
+    public EventsEntity(String current_date, String user, String action) {
+        this.current_date = current_date;
+        this.user = user;
+        this.action = action;
+    }
 }
