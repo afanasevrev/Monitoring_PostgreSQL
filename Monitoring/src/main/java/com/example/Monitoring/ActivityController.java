5cf67d3aa159a967ead1145c8cfb676f777730e9
revoly afanasev <rewoly@mail.ru>
2024-10-17 22:45:54 +0900

    Настройка мониторинга

--- Monitoring/src/main/java/com/example/Monitoring/ActivityController.java
+++ Monitoring/src/main/java/com/example/Monitoring/ActivityController.java
@@ -1,31 +1,32 @@
 package com.example.Monitoring;
-import org.springframework.scheduling.annotation.Scheduled;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-@Service
-public class ActivityMonitorService {
+@RestController
+public class ActivityController {
     @Autowired
     private DataSource dataSource;
-    @Scheduled(fixedRate = 6000)
-    public void collectActivityData(){
+    @GetMapping("/")
+    private String getInfo() {
         try (Connection connection = dataSource.getConnection();
              PreparedStatement stmt = connection.prepareStatement("SELECT * FROM pg_stat_activity");
              ResultSet rs = stmt.executeQuery()) {
              while (rs.next()) {
-                // Обработайте результаты и сохраните данные
                 String user = rs.getString("usename");
                 String query = rs.getString("query");
-                // Сохранение данных в свою таблицу
-                System.out.println(user + query);
-            }
+                System.out.println(rs.getString(15));
+             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return "GET-INFO";
     }
 }
