02f78d13f89f7f12f1394c8f17e8b4e41ebda59a
revoly afanasev <rewoly@mail.ru>
2024-10-20 21:55:30 +0900

    Работа с запросами в БД

--- Monitoring/src/main/java/com/example/Monitoring/ActivityController.java
+++ Monitoring/src/main/java/com/example/Monitoring/ActivityController.java
@@ -1,32 +1,16 @@
 package com.example.Monitoring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-@RestController
+@Controller
 public class ActivityController {
     @Autowired
     private DataSource dataSource;
     @GetMapping("/")
     private String getInfo() {
-        /**
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement stmt = connection.prepareStatement("SELECT * FROM pg_stat_activity");
-             ResultSet rs = stmt.executeQuery()) {
-             while (rs.next()) {
-                String user = rs.getString("usename");
-                String query = rs.getString("query");
-                System.out.println(rs.getString(15));
-             }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-         **/
         return "GET-INFO";
     }
 }
